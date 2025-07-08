aa9ca5d7a615a67fba05f9e0eb9adc01a3e17de8
yn2599 <yanmichael2504@gmail.com>
2025-07-08 10:28:14 -0300

    capturando o evento do formulário

--- scripts.js
+++ scripts.js
@@ -1,7 +1,18 @@
+// obtendo os elementos do formulário
+const form = document.querySelector('form')
 const amount = document.getElementById('amount')
+const currency = document.getElementById('currency')
 
 // Manipulando o input para receber somente números
 amount.addEventListener("input",() => {
-  console.log(amount.value)
+  const hascharacterRTegex = /\D+/g
 
+  console.log(amount.value)
+  amount.value = amount.value.replace(hascharacterRTegex, " ")
 })
+
+// Captando o evento do submit no formulário
+form.onsubmit = (event) => {
+  event.preventDefault()
+  console.log(currency.value)
+}
